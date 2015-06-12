31b4534a565933d478f570fd3592509966512599
TUKOMI-dev <mryund@gmail.com>
2015-06-12 09:13:04 +0800

    admin page

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,5 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +20,7 @@ module Blog1
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
